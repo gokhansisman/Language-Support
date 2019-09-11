6def1fffb902c842643e49c076234c31eaeead16
gokhansisman <pa1ngokhan@gmail.com>
2019-09-12 02:53:50 +0300

    Created Refs

--- clientApp/src/App.js
+++ clientApp/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-import  {Row, Col}  from 'reactstrap';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { Row, Col } from 'reactstrap';
 import logo from './logo.svg';
 import {
   PopupboxManager,
@@ -13,69 +13,84 @@ import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table';
 import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table.min.js';
 import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table.min.js';
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       data: [],
       deneme: false,
-      english:"",
-      turkish:"",
-      polish:"",
-      spanish:"",
-      sentences:""
-    // redirect: false
+      english: "",
+      turkish: "",
+      polish: "",
+      spanish: "",
+      sentences: ""
+      
+      // redirect: false
     };
-    this.update =this.update.bind(this);
-    this.postData =this.postData.bind(this);
-  };
-  update(e){
+    this.openPopupbox = this.openPopupbox.bind(this);
+    this.update = this.update.bind(this);
+    this.postData = this.postData.bind(this);
+    this.english = React.createRef();
+    this.turkish = React.createRef();
+    this.polish = React.createRef();
+    this.spanish = React.createRef();
+    this.sentences = React.createRef();
+
+
+    
+    
+  }; 
+  update() {
     const content = (
       <div>
-        <input className="quotes" type="text" onChange={this.update} placeholder="English"></input>
-        <input className="quotes" type="text" onChange={this.update} placeholder="Turkish"></input>
-        <input className="quotes" type="text" onChange={this.update} placeholder="Polish"></input>
-        <input className="quotes" type="text" onChange={this.update} placeholder="Spanish"></input>
-        <input type="text" className="quotes-from" onChange={this.update} placeholder="Sentences"></input>
+        <input className="quotes" type="text" ref={this.english}  onChange={this.openPopupbox} placeholder="English"></input>
+        <input className="quotes" type="text" ref={this.turkish} onChange={this.openPopupbox} placeholder="Turkish"></input>
+        <input className="quotes" type="text" ref={this.polish}  onChange={this.openPopupbox} placeholder="Polish"></input>
+        <input className="quotes" type="text" ref={this.spanish}  onChange={this.openPopupbox} placeholder="Spanish"></input>
+        <input className="quotes" type="text" ref={this.sentences}  onChange={this.openPopupbox} placeholder="Sentences"></input>
         <button className="quotes" onClick={this.postData} type="submit">Save</button>
       </div>
     )
     PopupboxManager.open({ content })
-    this.setState({ 
-      english:e.target.value,
-      turkish:e.target.value,
-      polish:e.target.value,
-      spanish:e.target.value,
-      sentences:e.target.value
-    })
+     }
+  openPopupbox(event) {
+    this.setState({
+      english: this.english.current.value,
+      turkish: this.turkish.current.value,
+      polish: this.polish.current.value,
+      spanish: this.spanish.current.value,
+      sentences: this.sentences.current.value
+
+    });
  
   }
-  openPopupbox() {
-    
-  }
-  componentDidMount(){
+  componentDidMount() {
     this.fetchData();
-    this.setState({deneme: true}, function () {
+    this.setState({ deneme: true }, function () {
       console.log(this.state.deneme);
     });
     this.fetchData();
   }
 
-  fetchData(){
+  fetchData() {
     fetch('http://localhost:8080/')
-    .then(response => response.json())
-    .then(json => {
-      console.log(json)
-      this.setState({
-        data : json,
-        deneme: true
-      },function() {
-        console.log(this.state.data);
-     })})
-      .catch(error => console.log('parsing failder',error))
-     
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        this.setState({
+          data: json,
+          deneme: true
+        }, function () {
+          console.log(this.state.data);
+        })
+      })
+      .catch(error => console.log('parsing failder', error))
+
   }
-  
+
   postData() {
+    console.log(this.state.turkish)
+    console.log(this.state.english)
+
     fetch(`http://localhost:8080/ekle`, {
       method: 'POST',
       headers: {
@@ -93,7 +108,7 @@ class App extends Component {
   }
 
 
-  
+
   render() {
     const popupboxConfig = {
       titleBar: {
@@ -103,24 +118,24 @@ class App extends Component {
       fadeIn: true,
       fadeInSpeed: 500
     }
-    
+
     return (
       <div>
-<button onClick={this.update}>Click Me</button>
-<PopupboxContainer{...popupboxConfig}></PopupboxContainer>
-              <BootstrapTable search exportCSV data={this.state.data}    scrollTop={ 'Bottom' }
-              tableStyle={ { border: '#0000FF 2.5px solid' } }
-              containerStyle={ { border: '#FFBB73 2.5px solid' } }
-              headerStyle={ { border: 'red 1px solid' } }
-              bodyStyle={ { border: 'green 1px solid' } } 
-              >
-                <TableHeaderColumn width='200' dataField='english' isKey>ENGLISH</TableHeaderColumn>
-                <TableHeaderColumn width='200' dataField='turkish'>TURKISH</TableHeaderColumn>
-                <TableHeaderColumn width='200' dataField='polish'>POLISH </TableHeaderColumn>
-                <TableHeaderColumn width='200' dataField='spanish'>SPANISH </TableHeaderColumn>
-                <TableHeaderColumn width='200' dataField='sentences'>SENTENCES</TableHeaderColumn>
-              </BootstrapTable>
-              </div>
+        <button onClick={this.update}>Click Me</button>
+        <PopupboxContainer{...popupboxConfig}></PopupboxContainer>
+        <BootstrapTable search exportCSV data={this.state.data} scrollTop={'Bottom'}
+          tableStyle={{ border: '#0000FF 2.5px solid' }}
+          containerStyle={{ border: '#FFBB73 2.5px solid' }}
+          headerStyle={{ border: 'red 1px solid' }}
+          bodyStyle={{ border: 'green 1px solid' }}
+        >
+          <TableHeaderColumn width='200' dataField='english' isKey>ENGLISH</TableHeaderColumn>
+          <TableHeaderColumn width='200' dataField='turkish'>TURKISH</TableHeaderColumn>
+          <TableHeaderColumn width='200' dataField='polish'>POLISH </TableHeaderColumn>
+          <TableHeaderColumn width='200' dataField='spanish'>SPANISH </TableHeaderColumn>
+          <TableHeaderColumn width='200' dataField='sentences'>SENTENCES</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
     );
   }
 }
