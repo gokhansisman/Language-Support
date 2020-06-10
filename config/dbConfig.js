d92699e19578005d71a1906235d89c7d6617d2a0
gokhansisman <pa1ngokhan@gmail.com>
2020-06-10 22:31:28 +0300

    config env added

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,3 +1,3 @@
-const options = { MONGO_ATLAS_PW: "7112129", }
+const options = { MONGO_ATLAS_PW: process.env.MONGO_ATLAS_PW, }
 options.MONGO_CONN_STR = 'mongodb://sisman:' + options.MONGO_ATLAS_PW + '@restful-api-node-shard-00-00-22grx.mongodb.net:27017,restful-api-node-shard-00-01-22grx.mongodb.net:27017,restful-api-node-shard-00-02-22grx.mongodb.net:27017/test?ssl=true&replicaSet=restful-api-node-shard-0&authSource=admin&retryWrites=true'  //  mongodb+srv://sisman:<password>@restful-api-node-22grx.mongodb.net/test?retryWrites=true&w=majority
 module.exports = options
