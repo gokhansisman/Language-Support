cfce3c58d6c659ce878e24315795cab89941397c
gokhansisman <pa1ngokhan@gmail.com>
2020-06-10 22:06:52 +0300

    config visible

---
+++ config/dbConfig.js
@@ -0,0 +1,3 @@
+const options = { MONGO_ATLAS_PW: "7112129", }
+options.MONGO_CONN_STR = 'mongodb://sisman:' + options.MONGO_ATLAS_PW + '@restful-api-node-shard-00-00-22grx.mongodb.net:27017,restful-api-node-shard-00-01-22grx.mongodb.net:27017,restful-api-node-shard-00-02-22grx.mongodb.net:27017/test?ssl=true&replicaSet=restful-api-node-shard-0&authSource=admin&retryWrites=true'  //  mongodb+srv://sisman:<password>@restful-api-node-22grx.mongodb.net/test?retryWrites=true&w=majority
+module.exports = options
