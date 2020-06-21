4d73b6169ed62da60a88730da96b89ca88fdaf49
gokhansisman <pa1ngokhan@gmail.com>
2020-06-22 00:09:23 +0300

    Some styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BootstrapTable, TableHeaderColumn, InsertButton } from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn, InsertButton } from 'react-bootstrap-table';
 import Header from './components/header'
 import { Row, Col } from 'reactstrap';
 import { Button } from 'react-bootstrap';
@@ -26,11 +26,12 @@ class App extends Component {
       polish: "",
       spanish: "",
       sentences: "",
-      error:false
+      error: false
       // redirect: false
     };
-    this.openPopupbox = this.openPopupbox.bind(this);
+    this.saveWords = this.saveWords.bind(this);
     this.update = this.update.bind(this);
+    this.translate = this.translate.bind(this);
     this.postData = this.postData.bind(this);
     this.validateInput = this.validateInput.bind(this);
     this.english = React.createRef();
@@ -47,156 +48,197 @@ class App extends Component {
     return true;
   }
 
-  update() {
+  translate() {
     const content = (
       <div>
         <input
           className="quotes" type="text" ref={this.english} onChange={e => {
             e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
-            this.openPopupbox()
+            this.translateWords()
           }
-          } 
+          }
           placeholder="English"></input>
         <input className="quotes" type="text"
           ref={this.turkish} onChange={
             e => {
               e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
-              this.openPopupbox()
+              this.translateWords()
             }
           } placeholder="Turkish"></input>
         <input className="quotes" type="text" ref={this.polish} onChange={
           e => {
             e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
-            this.openPopupbox()
+            this.translateWords()
           }
         } placeholder="Polish"></input>
         <input className="quotes" type="text" ref={this.spanish} onChange={
           e => {
             e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
-            this.openPopupbox()
+            this.translateWords()
           }
         } placeholder="Spanish"></input>
-        {/* <input className="quotes" type="text" ref={this.sentences}  onChange={this.openPopupbox} placeholder="Sentences"></input> */}
-        <button className="quotes" onClick={this.postData} type="submit">Save</button>
-      </div>
+        {/* <input className="quotes" type="text" ref={this.sentences}  onChange={this.saveWords} placeholder="Sentences"></input> */}
+        <div style={{
+          position: "relative", textAlign: "center"
+        }}>
+          <Button variant="dark" size="sm" style={{ color: 'olivedrab', margin: '5px', width: '%10' }} onClick={this.postData} type="submit">Translate</Button>
+        </div>
+      </div >
     )
     PopupboxManager.open({ content })
   }
-  openPopupbox(event) {
 
-      this.setState({
-        english: this.english.current.value.toLowerCase().replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇę]/gi, ""),
-        turkish: this.turkish.current.value.toLowerCase(),
-        polish: this.polish.current.value.toLowerCase(),
-        spanish: this.spanish.current.value.toLowerCase()
-
-      });
-    // }
-    // else {
-    //   alert("Please do not enter numbers !")
-
-    // }
-
-
-  }
-  createCustomInsertButton = (onClick) => {
-    return (
-      <div>
-      <Button variant="outline-success" onClick={this.update}>Add Word</Button>
+update() {
+  const content = (
+    <div>
+      <input
+        className="quotes" type="text" ref={this.english} onChange={e => {
+          e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
+          this.saveWords()
+        }
+        }
+        placeholder="English"></input>
+      <input className="quotes" type="text"
+        ref={this.turkish} onChange={
+          e => {
+            e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
+            this.saveWords()
+          }
+        } placeholder="Turkish"></input>
+      <input className="quotes" type="text" ref={this.polish} onChange={
+        e => {
+          e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
+          this.saveWords()
+        }
+      } placeholder="Polish"></input>
+      <input className="quotes" type="text" ref={this.spanish} onChange={
+        e => {
+          e.target.value = e.target.value.replace(/[^A-Za-z-ğüşöçİĞÜŞÖÇĄąĆćĘęŁłŃńÓóŚśŹźŻżÑñáÁéÉíÍóÓúÚ]/gi, "");
+          this.saveWords()
+        }
+      } placeholder="Spanish"></input>
+      {/* <input className="quotes" type="text" ref={this.sentences}  onChange={this.saveWords} placeholder="Sentences"></input> */}
+      <div style={{
+        position: "relative", textAlign: "center"
+      }}>
+        <Button variant="dark" size="sm" style={{ color: 'olivedrab', margin: '5px', width: '%10' }} onClick={this.postData} type="submit">Save</Button>
       </div>
-      //<button style={{ color: 'red' }} onClick={this.update}>Add rows</button>
-    );
-  }
-  componentDidMount() {
-    this.fetchData();
-    this.setState({ deneme: true }, function () {
-      console.log(this.state.deneme);
-    });
-    this.fetchData();
-  }
-//https://language-support.herokuapp.com/api
-  fetchData() {
-    fetch('https://language-support.herokuapp.com/api')
-      .then(response => response.json())
-      .then(json => {
-        console.log(json)
-        this.setState({
-          data: json,
-          deneme: true
-        }, function () {
-          console.log(this.state.data);
-        })
-      })
-      .catch(error => console.log('parsing failder', error))
+    </div >
+  )
+  PopupboxManager.open({ content })
+}
+saveWords(event) {
 
-  }
-//https://language-support.herokuapp.com/api/ekle
-  postData() {
-    fetch('https://language-support.herokuapp.com/api/ekle', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        english: this.state.english,
-        turkish: this.state.turkish,
-        polish: this.state.polish,
-        spanish: this.state.spanish,
-        sentences: this.state.sentences
-      })
-    }).then(res => res.json())
-    .catch(err=> {
-      console.log(err)
+  this.setState({
+    english: this.english.current.value.toLowerCase(),
+    turkish: this.turkish.current.value.toLowerCase(),
+    polish: this.polish.current.value.toLowerCase(),
+    spanish: this.spanish.current.value.toLowerCase()
+  });
+}
+translateWords() {
+  //translation.. here
+}
+
+createCustomInsertButton = (onClick) => {
+  return (
+    <div>
+      <Button variant="outline-success" onClick={this.update}>Add Word</Button>
+      <Button variant="outline-success" onClick={this.translate}>Use Google-Translate</Button>
+    </div>
+    //<button style={{ color: 'red' }} onClick={this.update}>Add rows</button>
+  );
+}
+componentDidMount() {
+  this.fetchData();
+  this.setState({ deneme: true }, function () {
+    console.log(this.state.deneme);
+  });
+  this.fetchData();
+}
+//https://language-support.herokuapp.com/api
+fetchData() {
+  fetch('https://language-support.herokuapp.com/api')
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
       this.setState({
-        error:true
+        data: json,
+        deneme: true
+      }, function () {
+        console.log(this.state.data);
       })
     })
-    if(this.state.error===true){
-     alert("Word could not add!")
-    }
-    PopupboxManager.close()
-   
+    .catch(error => console.log('parsing failder', error))
+
+}
+//https://language-support.herokuapp.com/api/ekle
+postData() {
+  fetch('https://language-support.herokuapp.com/api/ekle', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      english: this.state.english,
+      turkish: this.state.turkish,
+      polish: this.state.polish,
+      spanish: this.state.spanish,
+      sentences: this.state.sentences
+    })
+  }).then(res => res.json())
+    .catch(err => {
+      console.log(err)
+      this.setState({
+        error: true
+      })
+    })
+  if (this.state.error === true) {
+    alert("Word could not add!")
+  }
+  PopupboxManager.close()
+
 }
 
 
-  render() {
-    const popupboxConfig = {
-      titleBar: {
-        enable: true,
-        text: 'Adding New Word'
-      },
-      fadeIn: true,
-      fadeInSpeed: 500
-    }
-
-    const options = {
-      insertBtn: this.createCustomInsertButton
-    };
-
-    return (
-
-      <div style={{ width: 'auto' }}>
-        <Header />
-
-        <PopupboxContainer{...popupboxConfig}></PopupboxContainer>
-        <BootstrapTable search insertRow exportCSV data={this.state.data} scrollTop={'Bottom'}
-          options={options}
-          tableStyle={{ border: '#0000FF 2.5px solid' }}
-          containerStyle={{ border: '#FFBB73 2.5px solid' }}
-          headerStyle={{ border: 'red 1px solid' }}
-          bodyStyle={{ border: 'green 1px solid' }}
-        >
-
-          <TableHeaderColumn width='150' dataField='english' isKey>ENGLISH</TableHeaderColumn>
-          <TableHeaderColumn width='150' dataField='turkish'>TURKISH</TableHeaderColumn>
-          <TableHeaderColumn width='150' dataField='polish'>POLISH </TableHeaderColumn>
-          <TableHeaderColumn width='150' dataField='spanish'>SPANISH </TableHeaderColumn>
-          {/* <TableHeaderColumn width='150' dataField='sentences'>SENTENCES</TableHeaderColumn> */}
-        </BootstrapTable>
-      </div>
-    );
+render() {
+  const popupboxConfig = {
+    titleBar: {
+      enable: true,
+      text: 'Adding New Word'
+    },
+    fadeIn: true,
+    fadeInSpeed: 500
   }
+
+  const options = {
+    insertBtn: this.createCustomInsertButton
+  };
+
+  return (
+
+    <div style={{ width: 'auto' }}>
+      <Header />
+
+      <PopupboxContainer{...popupboxConfig}></PopupboxContainer>
+      <BootstrapTable search insertRow exportCSV data={this.state.data} scrollTop={'Bottom'}
+        options={options}
+        tableStyle={{ border: '#0000FF 2.5px solid' }}
+        containerStyle={{ border: '#FFBB73 2.5px solid' }}
+        headerStyle={{ border: 'red 1px solid' }}
+        bodyStyle={{ border: 'green 1px solid' }}
+      >
+
+        <TableHeaderColumn width='150' dataField='english' isKey>ENGLISH</TableHeaderColumn>
+        <TableHeaderColumn width='150' dataField='turkish'>TURKISH</TableHeaderColumn>
+        <TableHeaderColumn width='150' dataField='polish'>POLISH </TableHeaderColumn>
+        <TableHeaderColumn width='150' dataField='spanish'>SPANISH </TableHeaderColumn>
+        {/* <TableHeaderColumn width='150' dataField='sentences'>SENTENCES</TableHeaderColumn> */}
+      </BootstrapTable>
+    </div>
+  );
+}
 }
 
 
